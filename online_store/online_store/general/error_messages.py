3274eca3fec90e555f7ffbeca28c4af828b686b6
kostin-alex <aleksandr.kostin@gmail.com>
2024-09-08 16:16:19 +0300

    added error-messages

---
+++ online_store/online_store/general/error_messages.py
@@ -0,0 +1,14 @@
+"""
+Error messages with translations
+"""
+
+from django.utils.translation import gettext as _
+
+# Accounts
+USER_NOT_FOUND = _("User not found")
+PASSWORDS_DO_NOT_MATCH = _("Passwords do not match")
+
+
+# Products
+PRODUCT_NOT_FOUND = _("Guide not found")
+ACCESS_DENIED = _("Access denied. The user is not the manager")
